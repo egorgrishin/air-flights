959efafd36bf80993e6f23f9a68aa6f60b949a54
Egor Grishin <egor-grishin@bk.ru>
2024-06-14 18:41:45 +0300

    test

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -3,9 +3,6 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Searchers\PobedaSearch;
-use App\Searchers\SmartaviaSearch;
-use DateTime;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -13,19 +10,7 @@ class MainController
 {
     public function __invoke(Request $request, Response $response, $args): Response
     {
-
-//        $log = new Logger('name');
-//        $log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
-//
-//        $companies = (new CompanyRepository())->getAll();
-//        dd($companies);
-        return $response;
-        $dateTime = DateTime::createFromFormat('Y-m-d', '2024-06-15');
-        (new PobedaSearch())->run($dateTime);
-        (new SmartaviaSearch())->run($dateTime);
-
-        dd($request->getParsedBody());
-        $response->getBody()->write("Hello world! 77 ");
+        $response->getBody()->write("Hello world!");
         return $response;
     }
 }
