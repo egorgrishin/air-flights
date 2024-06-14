bac02ec21459b06a2a310f9df010b9d56eb19f31
Egor Grishin <egor-grishin@bk.ru>
2024-06-14 11:46:35 +0300

    remove old files

--- database/migrations/2_airport_company.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS airport_company
-(
-    airport_code VARCHAR(15)      NOT NULL,
-    company_id   TINYINT UNSIGNED NOT NULL,
-    PRIMARY KEY (airport_code, company_id),
-    FOREIGN KEY (airport_code) REFERENCES airports (code),
-    FOREIGN KEY (company_id) REFERENCES companies (id)
-)
