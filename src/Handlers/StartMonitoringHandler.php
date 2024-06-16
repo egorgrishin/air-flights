33a263ccc0f9654c79f8d574ee2bf9bcbeff0421
Egor Grishin <egor-grishin@bk.ru>
2024-06-16 23:51:02 +0300

    test bot fix

--- src/Handlers/StartMonitoringHandler.php
+++ src/Handlers/StartMonitoringHandler.php
@@ -23,7 +23,7 @@ final class StartMonitoringHandler implements HandlerContract
             $airport = [
                 [
                     'text'          => $airport->title,
-                    'callback_data' => "sel_dep:$airport->code:>:0",
+                    'callback_data' => "sel_arr:$airport->code:>:0",
                 ],
             ];
         }
