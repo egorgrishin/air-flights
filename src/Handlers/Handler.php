88c46c1076c081f134fdea6204bfc069b3b41056
Egor Grishin <egor-grishin@bk.ru>
2024-06-25 23:14:18 +0300

    ignore exceptions in answerCallbackQuery tg method

--- src/Handlers/Handler.php
+++ src/Handlers/Handler.php
@@ -8,6 +8,7 @@ use App\Contracts\HandlerContract;
 use App\Core\Telegram;
 use App\Dto\CallbackDto;
 use App\Enums\TelegramMethod;
+use Throwable;
 
 abstract readonly class Handler implements HandlerContract
 {
@@ -23,10 +24,7 @@ abstract readonly class Handler implements HandlerContract
         $this->parseDto($dto);
 
         if ($dto instanceof CallbackDto) {
-            $this->telegram->send(TelegramMethod::SendAnswer, [
-                'callback_query_id' => $dto->callbackQueryId,
-            ]);
-
+            $this->sendCallbackAnswer($dto->callbackQueryId);
             $this->method = TelegramMethod::Edit;
             $this->messageId = $dto->messageId;
         } else {
@@ -40,4 +38,18 @@ abstract readonly class Handler implements HandlerContract
     abstract public function process(): void;
 
     abstract protected function parseDto(DtoContract $dto): void;
+
+    /**
+     * Отправляет уведомление в Telegram об успешном получении запроса
+     */
+    private function sendCallbackAnswer(string $callbackQueryId): void
+    {
+        try {
+            $this->telegram->send(TelegramMethod::SendAnswer, [
+                'callback_query_id' => $callbackQueryId,
+            ]);
+        } catch (Throwable) {
+            //
+        }
+    }
 }
