5bed5bc5b16c4e6a982792abbb9cc712fe634586
Egor Grishin <egor-grishin@bk.ru>
2024-06-26 19:51:33 +0300

    update texts

--- src/Handlers/Subscription/SubscriptionHandler.php
+++ src/Handlers/Subscription/SubscriptionHandler.php
@@ -97,7 +97,7 @@ final readonly class SubscriptionHandler extends Handler
             $date = DateTime::createFromFormat('Y-m-d', $sub->date)->format('d.m.Y');
             $buttons[] = [
                 [
-                    'text'          => "$date Минеральные Воды — Минеральные Воды {$sub->minPrice}р.",
+                    'text'          => "$date Санкт-Петербург — Челябинск {$sub->minPrice}р.",
                     'callback_data' => "$this->selfState:$this->offset:$sub->id",
                 ],
             ];
